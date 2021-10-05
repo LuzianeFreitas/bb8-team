fcf9a151dbfca9fd0dcf98b93660ca4849f99b6d
Luziane Freitas <freitas.lu@outlook.com>
2021-10-04 22:13:58 -0300

    create register page

---
+++ src/components/registerForm/RegisterForm.vue
@@ -0,0 +1,80 @@
+<template>
+    <b-form>
+      <b-form-group
+        id="input-group-1"
+        label-for="name"
+        class="form-group"
+      >
+        <b-form-input
+          id="name"
+          type="name"
+          placeholder="name"
+          required
+        ></b-form-input>
+      </b-form-group>
+
+            <b-form-group
+        id="input-group-2"
+        label-for="email"
+        class="form-group"
+      >
+        <b-form-input
+          id="email"
+          type="email"
+          placeholder="email"
+          required
+        ></b-form-input>
+      </b-form-group>
+
+      <b-form-group id="input-group-3" label-for="password" class="form-group">
+        <b-form-input
+          id="password"
+          placeholder="password"
+          required
+        ></b-form-input>
+      </b-form-group>
+
+            <b-form-group id="input-group-4" label-for="confirmPassword" class="form-group">
+        <b-form-input
+          id="confirmPassword"
+          placeholder="confirm Password"
+          required
+        ></b-form-input>
+      </b-form-group>
+      
+      <b-form-group id="input-group-3" class="form-group">
+        <b-form-checkbox-group
+          id="logged"
+        >
+          <b-form-checkbox value="me">I accept the <strong>Terms of Use</strong> </b-form-checkbox>
+        </b-form-checkbox-group>
+      </b-form-group>
+
+      <b-button class="w-100 btn-register" type="submit">Register</b-button>
+    </b-form>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style>
+    .form-group {
+      padding-bottom: 1.8rem;
+    }
+
+    .btn-register {
+      background: var(--light-yellow);
+      border: 1px solid var(--light-yellow);
+
+      transition: filter 0.2s;
+    }
+
+    .btn-register:hover {
+      filter: brightness(0.9);
+      background: var(--light-yellow);
+      border: 1px solid var(--light-yellow);
+    }
+</style>
---
+++ src/views/Register.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="container d-flex justify-content-center align-items-center">
+        <div class="row w-25">
+            <img src="../assets/logo.svg" alt="Logo">
+            <h2 class="pt-3 text-center">Register</h2>
+            <register-form class="pt-3"/>
+            <p class="pt-4 pb-4 d-flex justify-content-center align-items-center">Já tem uma conta? <a href=""> <strong>Login</strong> </a></p>
+            <footer class="d-flex justify-content-center align-items-center"> <small> © 2021 BB8 Team. All rights reserved.</small></footer>
+        </div>  
+    </div>
+
+</template>
+
+<script>
+import RegisterForm from '../components/registerForm/RegisterForm'
+
+export default {
+    components: {
+        RegisterForm
+    }
+}
+</script>
+
+<style>
+    .container {
+        margin-top: 8rem;
+    }
+
+    img {
+        width: 6rem;
+        height: 6rem;
+    }
+
+    strong {
+        color: var(--light-orange);
+    }
+</style>
+
