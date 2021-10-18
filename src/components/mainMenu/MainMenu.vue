3b88af8c30dad84563acdcd63779659ec980b91b
Luziane Freitas <freitas.lu@outlook.com>
2021-10-18 16:51:55 -0300

    modified nav-bar

--- src/components/mainMenu/MainMenu.vue
+++ src/components/mainMenu/MainMenu.vue
@@ -1,17 +1,17 @@
 <template>
-<div class="shadow-sm w-100">
-  <b-navbar class="navbar">
+<div class="shadow-sm">
+  <b-navbar toggleable="lg" class="navbar">
     <b-navbar-brand href="#">
       <router-link to="/"><img class="d-inline-block align-top" src="../../assets/logo-menu.svg" alt="logo"></router-link>
     </b-navbar-brand>
 
     <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
 
-    <b-collapse class="d-flex justify-content-end" id="nav-collapse" is-nav>
-      <b-navbar-nav class="d-flex justify-content-between">
-        <b-nav-item class="btn-item"> <router-link to="/Home">Home</router-link></b-nav-item>
-        <b-nav-item class="btn-item"> <router-link to="/Register">Register</router-link></b-nav-item>
-        <b-nav-item class="btn-login-menu"> <router-link to="/Login">Login</router-link></b-nav-item>
+    <b-collapse id="nav-collapse" is-nav>
+      <b-navbar-nav right>
+        <b-nav-item class="btn-item text-center"> <router-link to="/Home">Home</router-link></b-nav-item>
+        <b-nav-item class="btn-item text-center"> <router-link to="/Register">Register</router-link></b-nav-item>
+        <b-nav-item class="btn-login-menu text-center"> <router-link to="/Login">Login</router-link></b-nav-item>
         <!-- <b-nav-item href="#">Disabled</b-nav-item> -->
       </b-navbar-nav>
     </b-collapse>
